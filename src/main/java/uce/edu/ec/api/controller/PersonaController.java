b53e1213217565f86571ffa5b57a05b7056a4bbb
Joel <jsespinosaa@uce.edu.ec>
2025-01-31 19:42:48 -0500

    Taller 28

--- src/main/java/uce/edu/ec/api/controller/PersonaController.java
+++ src/main/java/uce/edu/ec/api/controller/PersonaController.java
@@ -1,9 +1,13 @@
 package uce.edu.ec.api.controller;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
+
 import uce.edu.ec.api.service.IPersonaService;
 import uce.edu.ec.api.service.to.PersonaTo;
 
@@ -14,22 +18,37 @@ public class PersonaController{
 
     @GET
     @Path("/buscar")
-    public Response buscarPorId(){
+    public PersonaTo buscarPorId() {
         Integer id = 1;
-        PersonaTo persona= this.personaService.buscarPorId(id);
-        System.out.println(persona.getNombre());
-        return Response.ok(this.personaService.buscarPorId(id)).build();
+        PersonaTo persona = this.personaService.buscarPorId(id);
+        return persona;
     }
-    
-    public void guardar(PersonaTo persona){
+
+    @POST
+    @Path("/guardar")
+    public void guardar(PersonaTo persona) {
         this.personaService.guardar(persona);
     }
-    
-    public void actualizar(PersonaTo persona){
+
+    @PUT
+    @Path("/actualizar")
+    public void actualizar(PersonaTo persona) {
         this.personaService.actualizar(persona);
     }
     
-    public void borrar(Integer id){
+    @PATCH
+    @Path("/actualizar/parcial")   //Request body, solo puede haber uno, si quiero tener varios debo tener un objeto wraper
+    //el retorno de la capacidad se pone la respuesta del body del response
+    public void actualizarParcial(PersonaTo persona) {
+        PersonaTo tmp = this.personaService.buscarPorId(persona.getId());
+        tmp.setNombre(persona.getNombre());
+        this.personaService.actualizar(tmp);
+    }
+
+    @DELETE
+    @Path("/borrar")
+    public void borrar() {
+        Integer id = 1;
         this.personaService.borrar(id);
     }
 
