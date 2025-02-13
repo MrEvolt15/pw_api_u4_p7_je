2b011244687af3d08120281afdda067f113e51b2
Joel2231 <64055627+MrEvolt15@users.noreply.github.com>
2025-02-12 19:44:10 -0500

    Taller 33

--- src/main/java/uce/edu/ec/api/controller/PersonaController.java
+++ src/main/java/uce/edu/ec/api/controller/PersonaController.java
@@ -2,6 +2,7 @@ package uce.edu.ec.api.controller;
 
 import java.util.List;
 
+import jakarta.ws.rs.Produces;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
@@ -11,6 +12,8 @@ import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import uce.edu.ec.api.service.IPersonaService;
 import uce.edu.ec.api.service.to.PersonaTo;
 
@@ -21,9 +24,16 @@ public class PersonaController {
 
     @GET
     @Path("/{id}")
-    public PersonaTo buscarPorId(@PathParam("id") Integer id) {
-        
-        return this.personaService.buscarPorId(id);
+    @Produces(MediaType.APPLICATION_XML)
+    // http://localhost:8080/matriculaAPI/v1.1/personas/1
+    public Response buscarPorId(@PathParam("id") Integer id) {
+
+        //                        header, se pone el mensaje referente al codigo de estado personalizado
+        return Response.status(240).header("mensaje", "persona creada pero en validacion").header("valor1", 500)
+        .entity(this.personaService.buscarPorId(id)).build();
+        //return Response.status(Response.Status.OK).entity(this.personaService.buscarPorId(id)).build();
+        //return Response.ok(this.personaService.buscarPorId(id)).build();
+        //return this.personaService.buscarPorId(id);
     }
 
     @GET
@@ -42,7 +52,7 @@ public class PersonaController {
     }
 
     @GET
-    @Path("porNombreYApellido")
+    @Path("/porNombreYApellido")
     // http://localhost:8080/matriculaAPI/v1.1/personas/porNombreYApellido?nombre=Estuardo&apellido=Achig
     public List<PersonaTo> buscarPorNombreYApellido(@QueryParam("nombre") String nombre, @QueryParam("apellido") String apellido) {
         return this.personaService.buscarPorNombreYApellido(nombre, apellido);
--- src/main/java/uce/edu/ec/api/service/to/PersonaTo.java
+++ src/main/java/uce/edu/ec/api/service/to/PersonaTo.java
@@ -3,7 +3,10 @@ package uce.edu.ec.api.service.to;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
 public class PersonaTo implements Serializable{
     private Integer id;
     private String nombre;
@@ -22,24 +25,29 @@ public class PersonaTo implements Serializable{
     }
 
     // Getters and Setters
+    @XmlElement
     public Integer getId() {
         return id;
     }
+    
     public void setId(Integer id) {
         this.id = id;
     }
+    @XmlElement
     public String getNombre() {
         return nombre;
     }
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    @XmlElement
     public String getApellido() {
         return apellido;
     }
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
+    @XmlElement
     public LocalDateTime getFechaNacimiento() {
         return fechaNacimiento;
     }
